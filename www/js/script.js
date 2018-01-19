262a9169591289defd686da5b187bc87d4d62c84
ShAd0wInK <alexandredelaloy@live.com>
2018-01-19 10:30:29 +0100

    add some js

--- www/js/script.js
+++ www/js/script.js
@@ -1,28 +1,88 @@
-var btt = document.querySelector('.btt');
+// SCROLL ---------------------------------------
+	var btt = document.querySelector('.btt');
+		gotoHome = document.querySelector('.gotoHome');
+		gotoInfos = document.querySelector('.gotoInfos');
+		gotoProjects = document.querySelector('.gotoProjects');
+		gotoPro = document.querySelector('.gotoPro');
 
-btt.addEventListener('click', function() {
-	document.querySelector('#home').scrollIntoView({
-			behavior: 'smooth',
+	btt.addEventListener('click', function() {
+		document.querySelector('#home').scrollIntoView({
+				behavior: 'smooth',
+		});
 	});
-});
+	gotoHome.addEventListener('click', function() {
+		document.querySelector('#home').scrollIntoView({
+				behavior: 'smooth',
+		});
+	});
+	gotoInfos.addEventListener('click', function() {
+		document.querySelector('#infos').scrollIntoView({
+				behavior: 'smooth',
+		});
+	});
+	gotoProjects.addEventListener('click', function() {
+		document.querySelector('#projects').scrollIntoView({
+				behavior: 'smooth',
+		});
+	});
+	gotoPro.addEventListener('click', function() {
+		document.querySelector('#pro').scrollIntoView({
+				behavior: 'smooth',
+		});
+	});
 
-var menu = document.querySelector('.menu');
-	lineFirst = document.querySelector('.line-first');
-	lineCrossL = document.querySelector('.lineCrossL');
-	lineCrossR = document.querySelector('.lineCrossR');
-	lineShort = document.querySelector('.line-short');
-	menuOverlay = document.querySelector('.menuOverlay');
-	squares = document.querySelectorAll('.square'); 
+// HIDDEN MENU ----------------------------------
+	var menu = document.querySelector('.menu');
+		lineFirst = document.querySelector('.line-first');
+		lineCrossL = document.querySelector('.lineCrossL');
+		lineCrossR = document.querySelector('.lineCrossR');
+		lineShort = document.querySelector('.line-short');
+		menuOverlay = document.querySelector('.menuOverlay');
+		squares = document.querySelectorAll('.square'); 
+		menuList = document.querySelector('.menuList');
+		menuList_link = document.querySelectorAll('.menuList-link');
 
-menu.addEventListener('click', function() {
-	lineFirst.classList.toggle('lineFirst-anim');
-	lineCrossL.classList.toggle('lineCrossL-anim');
-	lineCrossR.classList.toggle('lineCrossR-anim');
-	lineShort.classList.toggle('line-short-anim');
-	menuOverlay.classList.toggle('menuOverlay-anim');
-	for (var i = 0; i < squares.length; i++) {
-		squares[i].classList.toggle('square-anim');
-	};
-	btt.classList.toggle('btt-anim');
-});
-	
+	menu.addEventListener('click', function() {
+		lineFirst.classList.toggle('lineFirst-anim');
+		lineCrossL.classList.toggle('lineCrossL-anim');
+		lineCrossR.classList.toggle('lineCrossR-anim');
+		lineShort.classList.toggle('line-short-anim');
+		menuOverlay.classList.toggle('menuOverlay-anim');
+		for (var i = 0; i < squares.length; i++) {
+			squares[i].classList.toggle('square-anim');
+		};
+		btt.classList.toggle('btt-anim');
+		for (var i = 0; i < menuList_link.length; i++) {
+			menuList_link[i].classList.toggle('appearText');
+		};
+
+	});
+	window.addEventListener('keydown', function(event) {
+	      	if (event.which === 27) {
+		      	lineFirst.classList.remove('lineFirst-anim');
+					lineCrossL.classList.remove('lineCrossL-anim');
+					lineCrossR.classList.remove('lineCrossR-anim');
+					lineShort.classList.remove('line-short-anim');
+					menuOverlay.classList.remove('menuOverlay-anim');
+					for (var i = 0; i < squares.length; i++) {
+						squares[i].classList.remove('square-anim');
+					};
+					btt.classList.remove('btt-anim');
+	      	}
+	  	});
+	menuList.addEventListener('click', function() {
+		lineFirst.classList.toggle('lineFirst-anim');
+		lineCrossL.classList.toggle('lineCrossL-anim');
+		lineCrossR.classList.toggle('lineCrossR-anim');
+		lineShort.classList.toggle('line-short-anim');
+		menuOverlay.classList.toggle('menuOverlay-anim');
+		for (var i = 0; i < squares.length; i++) {
+			squares[i].classList.toggle('square-anim');
+		};
+		btt.classList.toggle('btt-anim');
+	});
+
+
+
+
+		
